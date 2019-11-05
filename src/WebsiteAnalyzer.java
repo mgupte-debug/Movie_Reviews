d8905e01daaae4a91d1e8824e8cce9a5cf666be9
Alex Earl <arearl21@gmail.com>
2019-11-05 11:32:42 -0500

    indentation fix and mistake correction

--- src/WebsiteAnalyzer.java
+++ src/WebsiteAnalyzer.java
@@ -1,12 +1,10 @@
 package MovieReviews;
 
 public class WebsiteAnalyzer {
-    public class RottenTomatoes {
-        public String wordFinder(String message) {
-            int lineAbove = message.indexOf("<span class=\"mop - ratings - wrap__icon meter - tomato icon big medium - xs certified_fresh\"></span>");
-            while (message.charAt(lineAbove) != '%')
-                lineAbove++;
-            return message.substring(lineAbove, lineAbove + 4);
-        }
+    public String wordFinder(String message) {
+        int lineAbove = message.indexOf("<span class=\"mop - ratings - wrap__icon meter - tomato icon big medium - xs certified_fresh\"></span>");
+        while (message.charAt(lineAbove) != '%')
+            lineAbove++;
+        return message.substring(lineAbove, lineAbove + 4);
     }
 }
