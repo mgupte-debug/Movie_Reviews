05776691268c83d79184d26360ccca10d39ca183
Alex Earl <arearl21@gmail.com>
2019-11-07 12:58:29 -0500

    yay works

--- src/WebsiteAnalyzer.java
+++ src/WebsiteAnalyzer.java
@@ -6,11 +6,19 @@ public class WebsiteAnalyzer {
                 int lineAbove = message.indexOf("<span class=\"mop-ratings-wrap__icon meter-tomato icon big medium-xs certified_fresh\"></span>");
                 while (message.charAt(lineAbove) != '%')
                     lineAbove++;
-                return message.substring(lineAbove - 2, lineAbove + 1);
+
+                if(message.substring(lineAbove - 2, lineAbove + 1).equals("00%"))
+                    return "Rotten Tomatoes Score:" + message.substring(lineAbove - 3, lineAbove + 1);
+
+                return "Rotten Tomatoes Score:" + message.substring(lineAbove - 2, lineAbove + 1);
             }else{
             int lineAbove = message.indexOf("<span class=\"mop-ratings-wrap__icon meter-tomato icon big medium-xs fresh\"></span>");
             while (message.charAt(lineAbove) != '%')
                 lineAbove++;
+
+            if(message.substring(lineAbove - 2, lineAbove + 1).equals("00%"))
+                return "Rotten Tomatoes Score:" + message.substring(lineAbove - 3, lineAbove + 1);
+
             return "Rotten Tomatoes Score:" + message.substring(lineAbove - 2, lineAbove + 1);
             }
         }
