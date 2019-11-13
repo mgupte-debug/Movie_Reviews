5a7911c08283c1f25831c681221b82d6012ded2f
Mitali Gupte <57231694+mgupte-debug@users.noreply.github.com>
2019-11-12 21:55:22 -0500

    Update MovieMenu.java

--- src/MovieMenu.java
+++ src/MovieMenu.java
@@ -7,12 +7,29 @@ public class MovieMenu {
         urlConnectionTomatoes connectionTomatoes = new urlConnectionTomatoes();
 
         String a;
+        int b;
 
         while(true) {
+            
+            System.out.println("Movie or TV show");
+            String m = reader.nextLine();
+            m = m.toUpperCase();
+            
             a = "";
-            System.out.println("Input a movie name:");
+            System.out.println("Input a name:");
             a = reader.nextLine();
-            connectionTomatoes.getHTML(connectionTomatoes.trimMovieName(a));
+            
+            if(m.equals("TV")||m.equals("T")){
+            
+             System.out.println("Enter season: ");
+            b = reader.nextInt();
         }
+        else
+            b =0;
+            
+
+            
+            connectionTomatoes.getHTML(connectionTomatoes.trimName(a), b);
     }
+}
 }
